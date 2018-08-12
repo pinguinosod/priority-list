bc4da5004e0dac73ce5252e8afaea1b720b25a03
PinguinoSod <pinguino.sod@gmail.com>
2018-08-12 02:45:48 +0200

    Core functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'priority-list';
+  title = 'Priority List';
+  priorityList =  [
+                    {
+                      'title': 'The horse is pulent and piant',
+                      'rating': 4
+                    },
+                    {
+                      'title': 'Ich bin mehr als ein schönes Gesicht und ein guiler Körper',
+                      'rating': 2
+                    }
+                  ];
+  newItemName = '';
+
+  addItem() {
+    if (this.newItemName !== '') {
+      this.priorityList.push({'title': this.newItemName, 'rating': 1});
+      this.newItemName = '';
+    }
+  }
+
+  setRating(i, rating) {
+    this.priorityList[i].rating = rating;
+  }
+
+  removeItem(i) {
+    this.priorityList.splice(i, 1);
+  }
 }
